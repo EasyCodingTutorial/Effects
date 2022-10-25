b91c51564759433aebec332b93f26b145c30d41c
Easy Coding Tutorial <84335112+EasyCodingTutorial@users.noreply.github.com>
2022-10-25 23:11:08 +0530

    Add files via upload

---
+++ effects/index.js
@@ -0,0 +1,24 @@
+var swiper = new Swiper(".swiper", {
+  grabCursor: true,
+  loop: true,
+  spaceBetween: 20,
+  autoplay: {
+    delay: 2000,
+    disableOnInteraction: true,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  breakpoints: {
+    540: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: 3,
+    },
+  },
+});
